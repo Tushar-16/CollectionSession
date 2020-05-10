11156d3db8346f5d8062bf1102ded3c865aaa7fe
Tushar-16 <65106264+Tushar-16@users.noreply.github.com>
2020-05-10 16:48:04 +0530

    My First Commit

---
+++ src/ListConcept/ArrayListConcepts.java
@@ -0,0 +1,35 @@
+package ListConcept;
+
+import java.util.ArrayList;
+
+public class ArrayListConcepts {
+	
+	static{
+		
+		System.out.println("Static" );
+		
+	}
+
+	public static void main(String[] args) {
+
+		
+		ArrayList<String> ar1 = new ArrayList<String>(); // Generic in java 
+		
+		ar1.add("Tushar"); 
+		ar1.add("Hello"); 
+		ar1.add("Testing "); 
+		//ar1.add(123);  This will not allow as the Arraylist is String type 
+		
+		
+		ar1.add(1, "Naveen sir");
+		System.out.println(ar1.size());
+		ar1.remove(2);
+		
+	
+		System.out.println(ar1.size());
+		
+		
+
+	}
+
+}
